12012d8a81fba1723b6773d299481cf7702cbd60
Emerson <emerson.day@gmail.com>
2025-02-01 15:25:50 -0800

    gradient

--- OnboardingFlow/ContentView.swift
+++ OnboardingFlow/ContentView.swift
@@ -7,15 +7,19 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            WelcomePage()
+            FeaturesPage()
         }
-        .padding()
+        tabViewStyle(.page)
+          
     }
 }
 
